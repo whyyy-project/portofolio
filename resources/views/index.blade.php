558e21359da72ab760c4c136a0c0ac8b280b51b3
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2025-04-16 18:29:06 +0700

    edit index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -118,9 +118,7 @@
             <!-- Section Title -->
             <div class="container section-title" data-aos="fade-up">
                 <h2>About</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-                    consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-                    fugiat sit in iste officiis commodi quidem hic quas.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit illum distinctio pariatur.</p>
             </div><!-- End Section Title -->
 
             <div class="container" data-aos="fade-up" data-aos-delay="100">
