36bde2e7343db5d3682851f7a09e93ab7ba38ece
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2025-04-17 22:05:37 +0700

    resume on going

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -229,9 +229,9 @@
             <!-- Section Title -->
             <div class="container section-title" data-aos="fade-up">
                 <h2>Resume</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-                    consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-                    fugiat sit in iste officiis commodi quidem hic quas.</p>
+                <p>
+                    Berbagai pengalaman yang telah saya jalani selama ini
+                </p>
             </div><!-- End Section Title -->
 
             <div class="container">
@@ -239,69 +239,48 @@
                 <div class="row">
 
                     <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-                        <h3 class="resume-title">Sumary</h3>
-
-                        <div class="resume-item pb-0">
-                            <h4>Brandon Johnson</h4>
-                            <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing
-                                    and developing user-centered digital/print marketing material from initial concept
-                                    to final, polished deliverable.</em></p>
-                            <ul>
-                                <li>Portland par 127,Orlando, FL</li>
-                                <li>(123) 456-7891</li>
-                                <li>alice.barkley@example.com</li>
-                            </ul>
-                        </div><!-- Edn Resume Item -->
-
-                        <h3 class="resume-title">Education</h3>
+                        <h3 class="resume-title">Pendidikan</h3>
                         <div class="resume-item">
-                            <h4>Master of Fine Arts &amp; Graphic Design</h4>
-                            <h5>2015 - 2016</h5>
-                            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero
-                                voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
-                        </div><!-- Edn Resume Item -->
-
+                            <h4>S1 Teknik Informatika</h4>
+                            <h5>2021 - 2024 (Semester 3 - Lulus)</h5>
+                            <p><em>Universitas Nahdlatul Ulama Sunan Giri, Bojonegoro, Jawa Timur</em></p>
+                            <p>
+                                Melanjutkan pendidikan di Universitas Nahdlatul Ulama Sunan Giri Bojonegoro dengan konversi nilai dari Kampus sebelumnya, hingga Lulus dengan nilai IPK 3.75.
+                            </p>
+                        </div>
                         <div class="resume-item">
-                            <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-                            <h5>2010 - 2014</h5>
-                            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel
-                                ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae
-                                consequatur neque etlon sader mart dila</p>
+                            <h4>S1 Teknik Informatika</h4>
+                            <h5>2020 - 2021 (Semester 1-2)</h5>
+                            <p><em>Universitas Islam Lamongan, Lamongan, Jawa Timur</em></p>
+                            <p>
+                                Memutuskan pindah Universitas dengan alasan biaya hidup, kurangnya materi mengenai pengenalan pemrograman, lebih berfokus pada materi umum, dan sulitnya mendapatkan nilai yang baik.
+                            </p>
+                        </div><!-- Edn Resume Item -->
+                        <div class="resume-item">
+                            <h4>Rekayasa Perangkat Lunak</h4>
+                            <h5>2016 - 2019</h5>
+                            <p><em>SMK Negeri 1 Purwosari, Bojonegoro, Jawa Timur</em></p>
+                            <p>
+                                Pertama kali mengenal pemrograman, dan akhirnya memiliki minat mengenai pemrograman. Hanya saja saya memiliki keterbatasan perangkat laptop/komputer untuk melakukan pemrograman.
+                            </p>
                         </div><!-- Edn Resume Item -->
 
                     </div>
 
                     <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
-                        <h3 class="resume-title">Professional Experience</h3>
-                        <div class="resume-item">
-                            <h4>Senior graphic design specialist</h4>
-                            <h5>2019 - Present</h5>
-                            <p><em>Experion, New York, NY </em></p>
-                            <ul>
-                                <li>Lead in the design, development, and implementation of the graphic, layout, and
-                                    production communication materials</li>
-                                <li>Delegate tasks to the 7 members of the design team and provide counsel on all
-                                    aspects of the project. </li>
-                                <li>Supervise the assessment of all graphic materials in order to ensure quality and
-                                    accuracy of the design</li>
-                                <li>Oversee the efficient use of production project budgets ranging from $2,000 -
-                                    $25,000</li>
-                            </ul>
-                        </div><!-- Edn Resume Item -->
+                        <h3 class="resume-title">Pengalaman Kerja</h3>
 
                         <div class="resume-item">
-                            <h4>Graphic design specialist</h4>
-                            <h5>2017 - 2018</h5>
-                            <p><em>Stepping Stone Advertising, New York, NY</em></p>
+                            <h4>Warehouse Operator</h4>
+                            <h5>2019 - 2020</h5>
+                            <p><em>PT. Astra Honda Motor, Bekasi, Jawa Barat</em></p>
                             <ul>
-                                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations,
-                                    and advertisements).</li>
-                                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-                                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-                                <li>Created 4+ design presentations and proposals a month for clients and account
-                                    managers</li>
+                                <li>Melakukan inventarisasi stok sebelum proses produksi dimulai.</li>
+                                <li>Melaksanakan pemeriksaan kualitas (quality check) barang sebelum masuk ke lini produksi.</li>
+                                <li>Menyiapkan barang sesuai dengan perencanaan produksi harian.</li>
+                                <li>Menerapkan sistem FIFO (First In, First Out) untuk menjaga kualitas dan efisiensi alur barang.</li>
+                                <li>Melakukan inventarisasi stok setelah proses produksi selesai.</li>
+                                <li>Melaksanakan kegiatan stock opname secara berkala sesuai dengan jadwal yang telah ditentukan.</li>
                             </ul>
                         </div><!-- Edn Resume Item -->
 
