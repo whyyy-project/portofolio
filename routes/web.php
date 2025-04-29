b35b1c29d1238b277737ae6aeb1a64ff1611aa14
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2025-04-29 16:00:30 +0700

    implement authentication features with controllers, views, and routes for login, registration, password reset, and email verification

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use App\Http\Controllers\IndexController;
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/detail/{slug}', [IndexController::class, 'detailPortofolio'])->name('detail.portofolio');
 Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
