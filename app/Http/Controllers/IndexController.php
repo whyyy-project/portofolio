b0446ccad3ae60c788e59ffad118c47b117ac025
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2025-05-03 23:11:05 +0700

    fix: Update index method to fetch and shuffle portfolio data for improved display

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,14 @@ class IndexController extends Controller
 {
 public function index()
 {
-    $portofolio = PortofolioModel::all();
+      // Ambil data dengan id = 8
+    $utama = PortofolioModel::where('id', 8)->first();
+
+    // Ambil data lain selain id = 8, lalu acak
+    $lainnya = PortofolioModel::where('id', '!=', 8)->get()->shuffle();
+
+    // Gabungkan data: id 8 di awal, lainnya setelahnya
+    $portofolio = collect([$utama])->merge($lainnya);
     return view('index', compact('portofolio'));
 }
 
