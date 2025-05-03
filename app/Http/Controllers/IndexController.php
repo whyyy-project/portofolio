c1107780f5b1a72400f5cf102f93f1a02e268693
Wahyu Nur Cahyo <wahyu241200@gmail.com>
2025-05-03 23:03:03 +0700

    fix: Ensure image array is indexed correctly and shuffled in portfolio data

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -19,6 +19,8 @@ class IndexController extends Controller
                 fn($val) => trim($val) !== ''
             );
         }
+        $data->gambar = array_values($data->gambar); // pastikan index-nya urut
+        shuffle($data->gambar);
         // Kirim ke view 'index'
         return view('index', compact('portofolio'));
     }
